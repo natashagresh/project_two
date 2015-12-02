bf3454ccce04c76b1c1be31ea237d1e58ff17973
Natasha Gresh <natashagresh@hotmail.com>
2015-12-01 23:38:01 -0500

    added tables to schema.sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+drop table if exists person
+drop table if exists post
+
+create table person (id integer primary key, name VARCHAR, )
+
+create table post (id integer primary key, name VARCHAR, title_of_topic VARCHAR, time VARCHAR)
+
+
