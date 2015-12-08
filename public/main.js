a29c3af4b2b51add7b106181ef019ea135df051e
Natasha Gresh <natashagresh@hotmail.com>
2015-12-08 10:08:53 -0500

    changed main.js

--- public/main.js
+++ public/main.js
@@ -1,17 +1,17 @@
-console.log("im working");
+// console.log("im working");
 
-document.addEventListener("DOMContentLoaded", function() {
-	console.log("document loaded");
-	var a = document.querySelectorAll("a");
-	console.log(a);
-	function addClickListener(element) {
-		element.addEventListener("mouseover", function() {
-			console.log("moused over");
-			var fontColor = element.style.color;
-			fontColor === "grey" ? 
-		})
-	}
-	for(var i = 0; i < a.length; i++) {
-		addClickListener(a[i]);
-	}
-});
+// document.addEventListener("DOMContentLoaded", function() {
+// 	console.log("document loaded");
+// 	var a = document.querySelectorAll("a");
+// 	console.log(a);
+// 	function addClickListener(element) {
+// 		element.addEventListener("mouseover", function() {
+// 			console.log("moused over");
+// 			var fontColor = element.style.color;
+// 			fontColor === "grey" ? 
+// 		})
+// 	}
+// 	for(var i = 0; i < a.length; i++) {
+// 		addClickListener(a[i]);
+// 	}
+// });
